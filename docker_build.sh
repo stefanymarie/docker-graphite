bf6dd0f21ad271b82cd009772c93699b763212f1
Stefany Serino <stefanyserino@gmail.com>
2015-02-17 19:21:11 +0800

    tag with build number

--- docker_build.sh
+++ docker_build.sh
@@ -6,5 +6,5 @@ git checkout $VERSION
 VERSION_NAME=${VERSION#release\/}
 
 sudo docker build -t musashi/graphite:$VERSION_NAME .
-sudo docker tag musashi/graphite:$VERSION_NAME $DOCKER_BASE_URL/musashi/graphite:$VERSION_NAME
-sudo docker push $DOCKER_BASE_URL/musashi/graphite:$VERSION_NAME
+sudo docker tag musashi/graphite:$VERSION_NAME $DOCKER_BASE_URL/musashi/graphite:${VERSION_NAME}_${BUILD_NUMBER}
+sudo docker push $DOCKER_BASE_URL/musashi/graphite:${VERSION_NAME}_${BUILD_NUMBER}
